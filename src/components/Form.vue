5c8f5d17ea0cbe97a722cb54da0b9b673d5fd9fb
dimitribolokadze <89190087+anibolokadze@users.noreply.github.com>
2023-03-31 16:16:40 +0400

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,53 @@
+<template>
+  <Form @submit="onSubmit">
+    <Input
+      label="First Name"
+      type="text"
+      placeholder="First Name"
+      v-model="firstName"
+    />
+    <Input
+      label="Last Name"
+      type="text"
+      placeholder="Last Name"
+      v-model="lastName"
+    />
+    <Input
+      label="Email"
+      type="email"
+      placeholder="Email Address"
+      v-model="email"
+    />
+    <Input
+      label="Password"
+      type="password"
+      placeholder="Password"
+      v-model="password"
+    />
+    <button>CLAIM YOUR FREE TRIAL</button>
+  </Form>
+</template>
+
+<script>
+import { Input } from "./Input.vue";
+import { Form } from "vee-validate";
+export default {
+  name: "App",
+  components: { Input, Form },
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log(values);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Input.vue
@@ -0,0 +1,12 @@
+<template>
+  <Field />
+</template>
+<script>
+import { Field } from "vee-validate";
+export default {
+  name: "Input",
+  components: {
+    Field,
+  },
+};
+</script>
