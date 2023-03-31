e477056011b16bf51bbf0bfca26bb3cf372d6f2a
dimitribolokadze <89190087+anibolokadze@users.noreply.github.com>
2023-03-31 18:05:19 +0400

    validation, thank you msg

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,53 +1,107 @@
 <template>
-  <Form @submit="onSubmit">
-    <Input
-      label="First Name"
-      type="text"
-      placeholder="First Name"
-      v-model="firstName"
-    />
-    <Input
-      label="Last Name"
-      type="text"
-      placeholder="Last Name"
-      v-model="lastName"
-    />
-    <Input
-      label="Email"
-      type="email"
-      placeholder="Email Address"
-      v-model="email"
-    />
-    <Input
-      label="Password"
-      type="password"
-      placeholder="Password"
-      v-model="password"
-    />
-    <button>CLAIM YOUR FREE TRIAL</button>
-  </Form>
+  <div v-if="!showThankYouMessage">
+    <Form @submit="onSubmit" ref="form">
+      <Field
+        name="First Name"
+        type="text"
+        placeholder="First Name"
+        v-model="firstName"
+        :rules="validateFirstName"
+      />
+      <ErrorMessage name="First Name" />
+
+      <Field
+        name="Last Name"
+        type="text"
+        placeholder="Last Name"
+        v-model="lastName"
+        :rules="validateLastName"
+      />
+      <ErrorMessage name="Last Name" />
+
+      <Field
+        name="Email"
+        type="email"
+        placeholder="Email Address"
+        v-model="email"
+        :rules="validateEmail"
+      />
+      <ErrorMessage name="Email" />
+
+      <Field
+        name="Password"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+        :rules="validatePassword"
+      />
+      <ErrorMessage name="Password" />
+
+      <button>CLAIM YOUR FREE TRIAL</button>
+    </Form>
+  </div>
+  <div v-else>
+    <h1>Thank you</h1>
+  </div>
 </template>
 
 <script>
-import { Input } from "./Input.vue";
-import { Form } from "vee-validate";
+import { Field, Form, ErrorMessage } from "vee-validate";
 export default {
   name: "App",
-  components: { Input, Form },
+  components: { Form, Field, ErrorMessage },
   data() {
     return {
       firstName: "",
       lastName: "",
       email: "",
       password: "",
+      showThankYouMessage: false,
     };
   },
   methods: {
-    onSubmit(values) {
-      console.log(values);
+    onSubmit() {
+      this.$refs.form.validate().then((success) => {
+        if (success) {
+          this.showThankYouMessage = true;
+        }
+      });
+    },
+
+    validateEmail(value) {
+      if (!value) {
+        return "Email cannot be empty";
+      }
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return "Looks like this is not an email";
+      }
+      return true;
+    },
+    validateFirstName(value) {
+      if (!value) {
+        return "First Name cannot be empty";
+      }
+      return true;
+    },
+    validateLastName(value) {
+      if (!value) {
+        return "Last Name cannot be empty";
+      }
+      return true;
+    },
+
+    validatePassword(value) {
+      if (!value) {
+        return "Password cannot be empty";
+      }
+      const regex =
+        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+      if (!regex.test(value)) {
+        return "8 Characters long or longer. At least one lowercase letter, one uppercase letter, one digit, or symbol";
+      }
+      return true;
     },
   },
 };
 </script>
-
-<style></style>
